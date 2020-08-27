f8b84cbd03a626d31fa903a2479af7de0a9cdd64
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2020-08-27 17:14:56 +0530

    Add files via upload

---
+++ circularqueue.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#define size 5
+using namespace std;
+int queue[size];
+int front=-1, rear=-1;
+
+//insert any element
+void enqueue (int element)
+{
+	if((rear+1)%size==front)
+	{
+		cout<<"\nQueue overflow"<<endl;
+	}
+	else if(front==-1 && rear==-1)
+	{
+		cout<<"\nyour element "<<element<<" is added into queue"<<endl;
+		front=rear=0;
+		queue[rear]=element;
+	}
+	else
+	{
+		cout<<"\nyour element"<<element<<" is added into queue."<<endl;
+		rear=(rear+1)%size;
+		queue[rear]=element;
+	}
+}
+
+//to delete any element
+
+void dequeue (void)
+{
+	if(front==-1 && rear==-1)
+	{
+		cout<<"\nQueue underflow"<<endl;
+	}
+	else if(front==rear)
+	{
+		cout<<"\nyour element "<<queue[front]<<" is deleted"<<endl;
+		front=rear=-1;
+	}
+	else
+	{
+		cout<<"\nyour element "<<queue[front]<<" is deleted"<<endl;
+		front=(front+1)%size;
+	}
+}
+
+//display elements of queue
+void display (void)
+{
+	int i;
+	cout<<"\ndisplay: ";
+	if(front==-1 && rear==-1)
+	{
+		cout<<"\nQueue is empty"<<endl;
+	}
+	else 
+	{
+		for(i=front; i<=rear; i++)
+			{
+				cout<<"\t"<<queue[i];
+			}
+	}
+	
+}
+
+//to see front element
+void peek (void)
+{
+	if(front==-1 && rear==-1)
+	{
+		cout<<"\nQueue is empty"<<endl;
+	}
+	else
+	{
+		cout<<"\nyour front element is "<<queue[front]<<endl;
+	}
+}
+
+int main()
+{
+	enqueue(10);
+	enqueue(20);
+	enqueue(-5);
+	display();
+	peek();
+	dequeue();
+	peek();
+	display();
+	return 0;
+}
+
+
+
